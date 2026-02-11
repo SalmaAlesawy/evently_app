a468d2ae4eada13c4fcaa1bfe28480b5798fadad
Salma <salmaalesawy433@gmail.com>
2026-02-11 20:48:33 +0200

    English_arabic language dark_light theme cloud firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:eventapp/app_setting_provider/app_setting_provider.dart';
 import 'package:eventapp/core/Routes/app_router.dart';
+import 'package:eventapp/core/l10n/app_localizations.dart';
 import 'package:eventapp/core/theme/ThemeManager.dart';
+import 'package:eventapp/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'package:provider/provider.dart';
 
 import 'core/Routes/pages_route_names.dart';
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(ChangeNotifierProvider(
-    create: (_) => AppSettingProvider(),
+    create: (context) => AppSettingProvider(),
     child: const MyApp(),
   ));
 }
@@ -18,23 +26,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final provider=context.watch<AppSettingProvider>();
+    final provider = Provider.of<AppSettingProvider>(context, listen: false);
     return MaterialApp(
       title: 'Flutter Demo',
-      themeMode: AppSettingProvider.themeMode,
+      themeMode: provider.themeMode,
       theme: ThemeManager.getLightTheme(),
       darkTheme: ThemeManager.getDarkTheme(),
-      locale: provider.local,
-      supportedLocales: const[
-        Locale("en"),
-        Locale("ar")],
-    //   ], localizationsDelegates:  [
-    //   GlobalMaterialLocalizations.delegate,
-    //   GlobalWidgetsLocalizations.delegate,
-    //   GlobalCupertinoLocalizations.delegate,
-    // ],
+      locale: Locale(provider.currentLanguage),
       initialRoute: PageRouteName.splash,
       onGenerateRoute: AppRouter.onGenerateRoute,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       debugShowCheckedModeBanner: false,
     );
   }
